930694590fe22b2d72d86763cf9a76aba76cbd85
GustavoGoncalves <gustavo.neves@simova.com.br>
2024-03-08 18:55:51 -0300

    Chat Implemented

--- server/server.js
+++ server/server.js
@@ -1,137 +1,21 @@
-// server.js
-
+const express = require('express');
+const app = express();
 const http = require('http');
-const socketIo = require('socket.io');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const server = http.createServer();
-const io = new socketIo.Server(server, {
-    cors: {
-        // origin: 'http://127.0.0.1:8080/RAVE/'
-        origin: 'http://localhost:3000'
-    }
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
-
-const PORT = process.env.PORT || 3001;
 
 io.on('connection', (socket) => {
-    console.log('Novo usuário conectado');
-
-    // Evento para criar uma nova sala
-    socket.on('createRoom', (roomId) => {
-        if (!rooms[roomId]) {
-            rooms[roomId] = { users: {} };
-            socket.join(roomId);
-            socket.emit('roomCreated', roomId);
-            console.log(`Nova sala criada: ${roomId}`);
-        }
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
     });
+  });
 
-    // Evento para entrar em uma sala existente
-    socket.on('joinRoom', (roomId) => {
-        if (rooms[roomId]) {
-            socket.join(roomId);
-            socket.emit('roomJoined', roomId);
-            console.log(`Usuário entrou na sala: ${roomId}`);
-        } else {
-            socket.emit('roomNotFound');
-        }
-    });
-
-    // Evento para sincronizar a reprodução do vídeo
-    socket.on('syncVideo', (roomId, currentTime) => {
-        socket.to(roomId).emit('videoSynced', currentTime);
-    });
-
-    // Evento para desconectar
-    socket.on('disconnect', () => {
-        console.log('Usuário desconectado');
-        // Remova o usuário da sala quando ele se desconectar
-        for (const roomId in rooms) {
-            if (rooms[roomId].users[socket.id]) {
-                delete rooms[roomId].users[socket.id];
-                break;
-            }
-        }
-    });
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
-
-server.listen(PORT, () => {
-    console.log(`Servidor Socket.IO rodando na porta ${PORT}`);
-});
-
-module.exports = io;
-// export { io };
-
-
-
-
-/* #region  Backup */
-
-// // const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// // const app = express();
-// // const server = http.createServer(app);
-// const server = http.createServer();
-// // const io = socketIo(server);
-// const io = new socketIo.Server(server, {
-//     cors: {
-//         origin: 'http://127.0.0.1:8080/RAVE/'
-//         // origin: 'http://127.0.0.1:3000'
-//     }
-// })
-
-// const PORT = process.env.PORT || 3001;
-
-// // Estrutura para armazenar as salas e os usuários nelas
-// const rooms = {};
-
-// io.on('connection', (socket) => {
-//     console.log('Novo usuário conectado');
-
-//     // Evento para criar uma nova sala
-//     socket.on('createRoom', (roomId) => {
-//         if (!rooms[roomId]) {
-//             rooms[roomId] = { users: {} };
-//             socket.join(roomId);
-//             socket.emit('roomCreated', roomId);
-//             console.log(`Nova sala criada: ${roomId}`);
-//         }
-//     });
-
-//     // Evento para entrar em uma sala existente
-//     socket.on('joinRoom', (roomId) => {
-//         if (rooms[roomId]) {
-//             socket.join(roomId);
-//             socket.emit('roomJoined', roomId);
-//             console.log(`Usuário entrou na sala: ${roomId}`);
-//         } else {
-//             socket.emit('roomNotFound');
-//         }
-//     });
-
-//     // Evento para sincronizar a reprodução do vídeo
-//     socket.on('syncVideo', (roomId, currentTime) => {
-//         socket.to(roomId).emit('videoSynced', currentTime);
-//     });
-
-//     // Evento para desconectar
-//     socket.on('disconnect', () => {
-//         console.log('Usuário desconectado');
-//         // Remova o usuário da sala quando ele se desconectar
-//         for (const roomId in rooms) {
-//             if (rooms[roomId].users[socket.id]) {
-//                 delete rooms[roomId].users[socket.id];
-//                 break;
-//             }
-//         }
-//     });
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Servidor Socket.IO rodando na porta ${PORT}`);
-// });
-
-// module.exports = { io, server };
-/* #endregion */
